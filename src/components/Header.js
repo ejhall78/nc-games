18966fcd5713f23bd018417e5c72d98cf103fdcf
ed <ejhall78@gmail.com>
2021-08-31 14:17:03 +0100

    make title in header a link to home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom';
+
 export const Header = () => {
   return (
     <header className="Header">
-      <h1>nc-games</h1>
+      <Link to="/">
+        <h1>nc-games</h1>
+      </Link>
       <p>User Profile</p>
       <p>Search Bar</p>
     </header>
