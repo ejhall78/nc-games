83dbd359c01b93f1e5a26fcfe30d3f29714b860c
ed <ejhall78@gmail.com>
2021-08-31 11:54:35 +0100

    setup route for home page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import './App.css';
 import { Header } from './components/Header';
 import { NavBar } from './components/NavBar';
 import { ReviewsList } from './components/ReviewsList';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <NavBar />
-      <ReviewsList />
+      <Switch>
+        <Route exact path="/">
+          <ReviewsList />
+        </Route>
+      </Switch>
     </div>
   );
 }
